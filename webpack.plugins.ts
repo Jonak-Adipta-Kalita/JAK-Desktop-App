ce7a210ae532c143e56efd87e5b30485bb343741
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-12-04 10:46:20 +0530

    well... that didnt compiled the tailwind....

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,5 +1,4 @@
 import type IForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
-import { TailwindCSSWebpackPlugin } from "tailwindcss-webpack-plugin";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
@@ -7,8 +6,5 @@ const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require("
 export const plugins = [
     new ForkTsCheckerWebpackPlugin({
         logger: "webpack-infrastructure",
-    }),
-    new TailwindCSSWebpackPlugin({
-        entry: "./src/styles/globals.css",
     }),
 ];
