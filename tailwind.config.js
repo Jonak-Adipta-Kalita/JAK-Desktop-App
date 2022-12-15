33a5d85f57cd903ddebc103dc344ee0b5b80803b
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-12-15 17:38:26 +0530

    ....

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["**/*.{html,js,css}"],
+    content: ["**/*.{html,js,ts,css,jsx,tsx}"],
     theme: {
         extend: {
             fontFamily: {
