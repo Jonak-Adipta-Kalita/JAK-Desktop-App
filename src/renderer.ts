1c4bff7fcf5aa5a459a16b1e7718b2dc028437bd
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-12-16 18:07:41 +0530

    the switch theme button looks much more good now!!

--- src/renderer.ts
+++ src/renderer.ts
@@ -6,14 +6,26 @@ declare global {
     }
 }
 
-const toggleDarkModeBtn = document.getElementById("toggle-dark-mode")!;
+const themeToggleBtn = document.getElementById("theme-toggle")!;
+const themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+const themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
 
-toggleDarkModeBtn.addEventListener("click", async () => {
+if (
+    localStorage.getItem("color-theme") === "dark" ||
+    (!("color-theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+    themeToggleLightIcon.classList.remove("hidden");
+} else {
+    themeToggleDarkIcon.classList.remove("hidden");
+}
+
+themeToggleBtn.addEventListener("click", async () => {
+    themeToggleDarkIcon.classList.toggle("hidden");
+    themeToggleLightIcon.classList.toggle("hidden");
     const isDarkMode = await window.darkMode.toggle();
-    if (toggleDarkModeBtn.classList.contains("placeholder-mode"))
-        toggleDarkModeBtn.classList.remove("placeholder-mode");
-    toggleDarkModeBtn.classList.add(isDarkMode ? "light-mode" : "dark-mode");
-    toggleDarkModeBtn.classList.remove(isDarkMode ? "dark-mode" : "light-mode");
+    themeToggleBtn.classList.add(isDarkMode ? "dark-mode" : "light-mode");
+    themeToggleBtn.classList.remove(isDarkMode ? "light-mode" : "dark-mode");
 });
 
 import { render } from "./app";
